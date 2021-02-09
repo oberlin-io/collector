2c0f1bc1c1812258a6ca09d8f463ab8a8634f976
oberljn <oberljn@gmail.com>
2021-02-09 09:49:02 -0500

    Collector auth log class and methods -- not tested

---
+++ collectors/logs.py
@@ -0,0 +1,46 @@
+import os
+import yaml
+
+class AuthLog():
+
+    def __init__(self):
+        
+        with open('../var.yaml', 'r') as f:
+            self.var = yaml.safe_load(f.read())
+
+        self.authlog_path = '/var/log/auth.log'
+
+
+    def watch_authlog(self):
+        """By monitoring file last modified and tracking most recent index."""
+        
+        self.new_last_mod = os.path.getmtime(self.authlog_path)
+    
+        if self.new_last_mod > self.var['authlog_last_mod']:
+            return True
+        else:
+            return False
+    
+    
+    def set_authlog():
+        
+        with open(authlog_path) as f:
+            self.authlog = f.readlines()[var['authlog_index']:]
+
+
+    def set_last_mod(self):
+
+        self.var['authlog_last_mod'] = self.new_last_mod
+        
+        with open('../var.yaml', 'w') as f:
+            f.write(yaml.dump(self.var))
+
+
+    def get_user_session(self):
+        
+        pat = re.compile(r'^(\w{3}\s{2}\d+\s\d{2}:\d{2}:\d{2})\s.+systemd-logind\[(\d+)\]:\s.+user\s(\w+)\s.+uid=(\d+)')
+
+        for line in self.auth.readlines():
+            if 'systemd-logind[' in line:
+                m = pat.match(line)
+                
